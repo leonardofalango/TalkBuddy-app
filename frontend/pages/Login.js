ce5349cbc789bea6282bf0c32eb6bae20810c57b
leonardo.falango <leo_falango@hotmail.com>
2023-11-10 17:03:43 -0300

    add: react-native-camera to test

--- frontend/pages/Login.js
+++ frontend/pages/Login.js
@@ -1,10 +1,35 @@
-import { View, StyleSheet, useState} from 'react-native';
-const { Service } = require('../services/Service') // importing service
-
+import { View, StyleSheet, useState, requireNativeComponent} from 'react-native';
+const { Service } = require('../services/Service')
+import { RNCamera } from 'react-native-camera';
 
 export default function Login({ navigation }) {
     return (
         <View style={styles.container}>
+
+        <RNCamera
+            ref={ref => {
+                this.camera = ref;
+            }}
+            style={ styles.preview }
+            type={ RNCamera.Constants.Type.back }
+            flashMode={ RNCamera.Constants.FlashMode.on }
+            androidCameraPermissionOptions={{
+                title: 'Permission to use camera',
+                message: 'We need your permission to use your camera',
+                buttonPositive: 'Ok',
+                buttonNegative: 'Cancel',
+            }}
+            androidRecordAudioPermissionOptions={{
+                title: 'Permission to use audio recording',
+                message: 'We need your permission to use your audio',
+                buttonPositive: 'Ok',
+                buttonNegative: 'Cancel',
+            }}
+            onGoogleVisionBarcodesDetected={({ barcodes }) => {
+                console.log(barcodes);
+            }}
+        />
+
         </View>
     )
 }
