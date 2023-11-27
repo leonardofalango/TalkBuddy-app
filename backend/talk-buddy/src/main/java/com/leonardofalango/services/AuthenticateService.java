9cbd0f3543f27a2af2169ff9e2c32379c9f508c0
leonardofalango <leo_falango@hotmail.com>
2023-11-27 16:49:04 -0300

    remove: annotation.Value

--- backend/talk-buddy/src/main/java/com/leonardofalango/services/AuthenticateService.java
+++ backend/talk-buddy/src/main/java/com/leonardofalango/services/AuthenticateService.java
@@ -3,7 +3,6 @@ package com.leonardofalango.services;
 import java.sql.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
